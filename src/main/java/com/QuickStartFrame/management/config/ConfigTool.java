1f30001f5be254b70c8af3816c686d38c4f40536
fengyonggang <18366132455@163.com>
2016-09-26 17:31:19 +0800

    document upload

--- src/main/java/com/QuickStartFrame/management/config/ConfigTool.java
+++ src/main/java/com/QuickStartFrame/management/config/ConfigTool.java
@@ -1,16 +1,16 @@
 package com.QuickStartFrame.management.config;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.InitializingBean;
 
 /**
  * 配置信息的工具类，在spring-mybatis文件的bean中配置
  */
 public class ConfigTool implements InitializingBean{
+	public static final Logger logger  = Logger.getLogger(ConfigTool.class);
 	
-
-
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		System.err.println("Bean初始化完成");
+		logger.info("bean初始化完成");
 	}
 }
